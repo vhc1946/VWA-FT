063a0154ff4ab3036c772d06cb92c9f93a826084
cvogel3434 <cvogel.a.m@gmail.com>
2022-11-01 14:15:22 -0500

    add vhc-component.js

--- Tech/bin/js/vg-dash.js
+++ Tech/bin/js/vg-dash.js
@@ -1,13 +1,37 @@
-import {wolstore} from '../../js/lstore.js';
-import {dashdom,wodom} from '../bin/back/ticket-dom.js';
-import {ServiceWO} from '../bin/back/sticket-build.js';
 
-import {DropNote} from '../bin/repo/js/vg-poppers.js';
-import {FINDparentele} from '../bin/repo/js/vg-displaytools.js';
 
-import {ADDmactions,ADDqactions} from '../bin/repo/js/vg-titlebar.js';
+import {wolstore} from './lstore.js';
+import {dashdom,wodom} from '../back/ticket-dom.js';
+import {ServiceWO} from '../back/sticket-build.js';
+
+import {DropNote} from '../repo/js/vg-poppers.js';
+import {FINDparentele} from '../repo/js/vg-displaytools.js';
+
+//import {ADDmactions,ADDqactions} from '../repo/js/vg-titlebar.js';
+
+import {CreateComponent} from '../repo/tools/vhc-components.js';
+
+var tdom = {
+  '#firstid.div':{
+    attributes:{
+      class:'some class'
+    },
+    children:{
+      '.firstclass.div':{
+        attributes:{
+          class:'someclass andanother'
+        }
+      }
+    }
+  }
+}
+
+document.body.appendChild(CreateComponent(tdom));
+
 
 var RROOT='../bin/repo/';
+
+
 
 var LOADwolist = ()=>{   //Loads WO list into display table
     let wolist = JSON.parse(localStorage.getItem(wolstore.techwo));
@@ -17,7 +41,7 @@ var LOADwolist = ()=>{   //Loads WO list into display table
         for(let x=0;x<wolist.length;x++){
             let item = document.createElement('div');
             item.classList.add(dashdom.list.item.cont);
-            
+
             item.appendChild(document.createElement('div')).innerText = wolist[x].num;
             item.lastChild.classList.add(dashdom.list.item.num);
 
@@ -35,7 +59,7 @@ var LOADwolist = ()=>{   //Loads WO list into display table
                 //Open de WO!
                 DropNote('tr',`WO # ${row.children[0].innerText} Loaded..`,'green');
             }
-            
+
         });
     }
 }
--- Tech/bin/repo/tools/vhc-components.js
+++ Tech/bin/repo/tools/vhc-components.js
@@ -4,8 +4,27 @@
 //add element
 
 
-export var CreateComponent=(comtree={})=>{
+export var CreateComponent=(comtree={},parent=null)=>{
+  console.log(comtree);
+  let newele;
   for(let ct in comtree){
-    console.log(ct[0])
+    newele = document.createElement(ct.split('.')[ct.split('.').length-1]);
+    switch(ct[0]){
+      case '#':{ newele.id=ct.split('.')[0].split('#')[1]; break;} //initialize id
+      case '.':{ newele.classList.add(ct.split('.')[1]);break;}
+    }
+    if(comtree[ct].attributes){
+      for(let ca in comtree[ct].attributes){
+        if(ca=='class'){newele.classList.add(...comtree[ct].attributes[ca].split(' '))}
+        else{newele.setAttribute(ca,comtree[ct].attributes[ca]);}
+      }
+    }
+    if(!parent){parent = newele}
+    if(comtree[ct].children){
+      for(let childs in comtree[ct].children){
+        parent.appendChild(CreateComponent({[childs]:comtree[ct].children[childs]},newele));
+      }
+    }
+    return newele;
   }
 }
