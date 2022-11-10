3e344a2fb724c9b7d73faad627e986a96d4287a2
cvogel3434 <cvogel.a.m@gmail.com>
2022-11-10 15:22:09 -0600

    restructuring of view controlls. Created a one class solution for the module

--- Tech/bin/repo/js/view-controller.js
+++ Tech/bin/repo/js/view-controller.js
@@ -12,8 +12,6 @@ var FINDparentele=(ele,stop)=>{
 
 /////////////////////
 
-
-
 //  Setup Module ///////////////////////////////////////////////////////////////
 var SETUPviewcontroller=(pfolder)=>{
   pubfolder=pfolder;
@@ -152,183 +150,139 @@ var vcgroup={//STYLE GROUPS
 }
 
 //  PUBLIC FUNCTIONS ///////////////////////////////////////////////////////////
-
 /* Initialize a set of views
     Will take a container and setup anything found in its first child, so nested
     views will not be setup
 */
-var SETUPviews=(cont,type='',style=null)=>{
-  let buttons = cont.getElementsByClassName(vcdom.menu.cont)[0].getElementsByClassName(vcdom.menu.button); //get the menu for the passed container
-  if(vcgroup[type]!=undefined){SETUPviewgroup(cont,type);}
+class ViewGroup{
+  constructor({cont=document.ElementCreate('div'),type='',style=null,delEve=()=>{},swtchEve=()=>{}}){
+    this.cont=CreateComponent(vdom());
+    cont.appendChild(this.cont);
 
-  RESETviews(cont); //make sure the view is defaulted
+    this.port=this.cont.getElementsByClassName(vcdom.port.cont)[0];
+    this.menu=this.cont.getElementsByClassName(vcdom.menu.cont)[0];
+    this.buttons=this.menu.children[0]; //to get navigation menu
 
-  for(let x=0;x<buttons.length;x++){ //loop through the menu items
-    let view = FINDview(cont,buttons[x].title);
-      if(x==0){
-        buttons[x].classList.add(vcdom.menu.selected);
-        view.classList.add(vcdom.port.selected);
-      }
-    if(view&&view!=undefined){
-      buttons[x].addEventListener('click',(ele)=>{
-        if(ele.target.classList.contains(vcdom.menu.button)){SWITCHview(view,ele.target);}
-      });
-    }
+    if(vcgroup[type]!=undefined){this.SETUPviewgroup(type);}
+
+    this.swtchEve=swtchEve;
+    this.delEve=delEve;
   }
-}
 
-var CREATEviewport=(cont,type='',actions=null,style=null)=>{  // Creates new Viewport setup
-  let RROOT = '';
-  let ele = CreateComponent(vdom(RROOT));
-  console.log(ele);
-}
-
-var CREATEvp=(cont,type='',actions=null,style=null)=>{  // OLD creation function
-  cont.classList.add(vcdom.cont);
-  let ele = document.createElement('div');
-  ele.classList.add(vcdom.menu.cont);
-  ele.appendChild(document.createElement('div'));
-  ele.appendChild(document.createElement('div'));
-  ele.lastChild.classList.add(vcdom.menu.qactions);
-  cont.appendChild(ele);
-  ele=document.createElement('div');
-  ele.classList.add(vcdom.port.cont);
-  cont.appendChild(ele);
-  SETUPviews(cont,type,style);
-}
-
-/* ADD a view
-    PASS:
-    - name - name of view
-    - view - html collection to add to view
-    - cont - name of container holding the tnv views and nav
-*/
-var ADDview=(name,view,cont,del=false,delEve=()=>{})=>{
-  if(ROOT){// Check if repo root is setup
-    try{
-      RESETviews(cont)
-    }catch{
-      console.log('Container id:',cont.id,' May NOT Exist...');
+  /* ADD a view
+      PASS:
+      - name - name of view
+      - view - html collection to add to view
+      - cont - name of container holding the tnv views and nav
+  */
+  ADDview(name,view,del=false){
+    try{this.RESETviews();}
+    catch{
+      console.log('Container id:',this.cont.id,' May NOT Exist...');
       return null;
     }
     view.title=name;
     view.classList.add(vcdom.port.view,vcdom.port.selected);
     view.prepend(document.createElement('div'));
-    view.firstChild.classList.add('viewcontrol-print-header',vcdom.menu.button,vcdom.menu.selected);
-    view.firstChild.innerText = name;
-    cont.getElementsByClassName(vcdom.port.cont)[0].appendChild(view);
+    this.port.appendChild(view);
 
     var button = document.createElement('div');//create tab button and add
     button.innerText = name
     button.title = name; //change to .title
     button.classList.add(vcdom.menu.button,vcdom.menu.selected);
+
     button.addEventListener('click',(ele)=>{
-      if(ele.target.classList.contains(vcdom.menu.button)){SWITCHview(view,ele.target);}
+      if(ele.target.classList.contains(vcdom.menu.button)){
+        this.SWITCHview(view,ele.target);
+      }
     });
     if(del){
-      button.appendChild(document.createElement('img')).src = ROOT + assets.viewclose;
+      button.appendChild(document.createElement('img')).src ='';
       button.lastChild.classList.add(vcdom.util.close);
       button.lastChild.addEventListener('dblclick',(ele)=>{
         ele=FINDparentele(ele.target,vcdom.menu.button);
         if(ele){
           console.log('Closing view..');
-          delEve(view);
-          REMOVEview(ele,cont);
+          this.delEve(); //optional delete process
+          this.REMOVEview(ele);
         }
       });
     }
-
-    cont.getElementsByClassName(vcdom.menu.cont)[0].children[0].appendChild(button);
+    this.buttons.appendChild(button);
     return view;
-  }else{
-    console.log('Module May not be setup...')
+  }
+
+  /* Remove the Selected tab
+    Set as a click listener in tab button
+
+    Deletes the selected tab button and
+    the associated view
+  */
+  REMOVEview(button){
+    var reset = false;
+    if(button.classList.contains(vcdom.menu.selected)){reset=true}
+
+    this.port.removeChild(this.FINDview(this.cont,button.title));
+    this.menu.removeChild(button);
+    if(reset){
+      try{this.port.children[this.port.children.length-1].classList.add(vcdom.port.selected);}catch{}
+      try{this.menu.children[this.menu.children.length-1].classList.add(vcdom.menu.selected);}catch{}
+      if(this.port.children.length==0){ //add default view
+        //port.appendChild(document.createElement('div'));
+        //port.lastChild.innerText = 'SELECT VIEW';
+        //port.lastChild.classList.add(vcdom.port.selected);
+      }
+    }
+  }
+
+  /* Reset the views
+      Will return the port and menu to default (no menu OR port items selected)
+  */
+  RESETviews(){
+    let buttons = this.menu.getElementsByClassName(vcdom.menu.selected);
+    let views = this.port.children;
+    for(let x=0;x<buttons.length;x++){buttons[x].classList.remove(vcdom.menu.selected);}
+    for(let x=0;x<views.length;x++){views[x].classList.remove(vcdom.port.selected);}
+  }
+
+  /* Search for a port in a container
+      Loops through the ports of a container and tries to match the ports.title
+      with the name passed.
+  */
+  FINDview(name){
+    let views = this.port.children;
+    for(let x=0;x<views.length;x++){if(views[x].title==name){return views[x]}}
     return null;
   }
 
-}
-
-var FINDbutton=(name,cont)=>{
-  console.log(cont);
-  let menu = cont.getElementsByClassName(vcdom.menu.cont)[0].getElementsByClassName(vcdom.menu.button);
-  for(let x=0;x<menu.length;x++){
-    if(menu[x].title==name){return menu[x]}
-  }
-  return null;
-}
-////////////////////////////////////////////////////////////////////////////////
-//  PRIVATE FUNCTIONS //////////////////////////////////////////////////////////
-
-var SETUPviewgroup=(cont,grp)=>{
-  cont.classList.add(vcgroup[grp].cont);
-  cont.getElementsByClassName(vcdom.menu.cont)[0].classList.add(vcgroup[grp].menu.cont);
-}
-
-/* Remove the Selected tab
-  Set as a click listener in tab button
-
-  Deletes the selected tab button and
-  the associated view
-*/
-var REMOVEview=(button,cont)=>{
-  var port = cont.getElementsByClassName(vcdom.port.cont)[0];
-  var menu = button.parentNode;
-  var reset = false;
-  if(button.classList.contains(vcdom.menu.selected)){reset=true}
-
-  port.removeChild(FINDview(cont,button.title));
-  menu.removeChild(button);
-  if(reset){
-    try{port.children[port.children.length-1].classList.add(vcdom.port.selected);}catch{}
-    try{menu.children[menu.children.length-1].classList.add(vcdom.menu.selected);}catch{}
-    if(port.children.length==0){ //add default view
-      //port.appendChild(document.createElement('div'));
-      //port.lastChild.innerText = 'SELECT VIEW';
-      //port.lastChild.classList.add(vcdom.port.selected);
+  FINDbutton(name){
+    let buts = this.buttons.children;
+    for(let x=0;x<this.buts.length;x++){
+      if(this.buts[x].title==name){return this.buts[x]}
     }
+    return null;
+  }
+
+  SWITCHview(view,button){
+    this.swtchEve(view.title);//optional switch function
+    this.RESETviews();
+    button.classList.add(vcdom.menu.selected);
+    view.classList.add(vcdom.port.selected);
+  }
+
+  SETUPviewgroup(grp){
+    this.cont.classList.add(vcgroup[grp].cont);
+    this.menu.classList.add(vcgroup[grp].menu.cont);
+  }
+
+  CLEARview(){
+    this.menu.children[0].innerHTML='';
+    this.port.innerHTML = '';
   }
 }
 
-/* Reset the views
-    Will return the port and menu to default (no menu OR port items selected)
-*/
-var RESETviews=(cont)=>{
-  let buttons = cont.getElementsByClassName(vcdom.menu.cont)[0].getElementsByClassName(vcdom.menu.selected);
-  let views = cont.getElementsByClassName(vcdom.port.cont)[0].children;
-  for(let x=0;x<buttons.length;x++){buttons[x].classList.remove(vcdom.menu.selected);}
-  for(let x=0;x<views.length;x++){views[x].classList.remove(vcdom.port.selected);}
-}
-
-/* Search for a port in a container
-    Loops through the ports of a container and tries to match the ports.title
-    with the name passed.
-*/
-var FINDview=(cont,name)=>{
-  let views = cont.getElementsByClassName(vcdom.port.cont)[0].children;
-  for(let x=0;x<views.length;x++){if(views[x].title==name){return views[x]}}
-  return null;
-}
-
-var SWITCHview=(view,button)=>{
-  RESETviews(view.parentNode.parentNode);
-  button.classList.add(vcdom.menu.selected);
-  view.classList.add(vcdom.port.selected);
-}
-
-var CLEARview=(cont)=>{
-  cont.getElementsByClassName(vcdom.menu.cont)[0].children[0].innerHTML='';
-  cont.getElementsByClassName(vcdom.port.cont)[0].innerHTML = '';
-}
-////////////////////////////////////////////////////////////////////////////////
 export {
   vcdom,
-  CREATEviewport,
-  SETUPviews,
-  ADDview,
-  FINDbutton,
-  FINDview,
-  REMOVEview,
-  SWITCHview,
-  CLEARview,
   SETUPviewcontroller,
-  FINDparentele
+  ViewGroup
 };
