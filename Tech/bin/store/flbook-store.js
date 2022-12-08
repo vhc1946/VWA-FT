2d33529a9dded29a5f9ac8b53e33f9eedc7d5276
cvogel3434 <cvogel.a.m@gmail.com>
2022-12-08 15:22:24 -0600

    load flatrate in browser

--- Tech/bin/store/flbook-store.js
+++ Tech/bin/store/flbook-store.js
@@ -1,11 +1,38 @@
 var fbdb;
 var ftable = JSON.parse(localStorage.getItem('flbook'));
+import {SENDrequestapi} from '../repo/apis/vapi/vapicore.js';
 import {IDBinterface} from '../repo/storage/IDBinterface.js';
 
+/*
+SENDrequestapi({
+  collect:"jonas",
+  store:'SERVICE',
+  db:'flbook',
+  method:'insert',
+  options:{docs:ftable}
+}).then(answr=>{});
+*/
 var fbdbsetup =(db)=>{
   let ostore = db.createObjectStore('books',{
     autoIncrement:true
   });
+  SENDrequestapi({
+    collect:"jonas",
+    store:'SERVICE',
+    db:'flbook',
+    method:'query',
+    options:{query:{}}
+  }).then(
+    answr=>{
+      console.log(answr)
+      if(answr.success){
+        let list = answr.body.result;
+        console.log('>>>>>>',list);
+        let trans=db.transaction('books','readwrite').objectStore('books');
+        for(let x=0;x<list.length;x++){trans.add(list[x]);}
+      }
+    }
+  );
 }
 export var fbstore = new IDBinterface('jonas-flatratebook','books',fbdbsetup);
 
