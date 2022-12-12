fca484f6964902b5d34b2b63259e4595d793fe18
cvogel3434 <cvogel.a.m@gmail.com>
2022-12-12 14:45:02 -0600

    move presentation signature functions to own file -> service-presentation.js

--- Tech/bin/vg-ticket.js
+++ Tech/bin/vg-ticket.js
@@ -83,48 +83,6 @@ document.getElementsByClassName('min-page-hide-button')[0].addEventListener('cli
   $(document.getElementsByClassName('min-page-cont')[0]).toggle();
 });
 
-
-/*
-const canvas = document.getElementsByClassName('signature-pad')[0];
-console.log(canvas.offsetTop);
-const ctx = canvas.getContext("2d");
-let coord = { x: 0, y: 0 };
-
-var resize=()=>{
-  ctx.canvas.width = canvas.width;
-  ctx.canvas.height = canvas.height;
-}
-var reposition=(event)=>{
-  coord.x = event.offsetX;
-  coord.y = event.offsetY;
-}
-var start=(event)=>{
-  document.addEventListener("mousemove", draw);
-  reposition(event);
-  console.log(event);
-}
-var stop=()=>{
-  document.removeEventListener("mousemove", draw);
-}
-var draw=(event)=>{
-  ctx.beginPath();
-  ctx.lineWidth = 4;
-  ctx.lineCap = "round";
-  ctx.strokeStyle = "black";
-  ctx.moveTo(coord.x, coord.y);
-  reposition(event);
-  ctx.lineTo(coord.x, coord.y);
-  ctx.stroke();
-}
-
-canvas.addEventListener("mousedown", start);
-canvas.addEventListener("mouseup", stop);
-window.addEventListener("resize", resize);
-
-resize();
-*/
-
-
 const canvas = document.getElementsByClassName('signature-pad')[0];
 const ctx = canvas.getContext('2d');
 
