28a704fba1d56db2d2dacc4853c7574768d85c5e
Ryan Murphy <ryanm@vogelheating.com>
2022-12-12 11:10:40 -0600

    added first pass at signature pad

--- Tech/bin/vg-ticket.js
+++ Tech/bin/vg-ticket.js
@@ -82,3 +82,89 @@ $(document.getElementById(titlebar.tbdom.page.settings)).hide();
 document.getElementsByClassName('min-page-hide-button')[0].addEventListener('click', (ele)=>{   // Home Button
   $(document.getElementsByClassName('min-page-cont')[0]).toggle();
 });
+
+
+/*
+const canvas = document.getElementsByClassName('signature-pad')[0];
+console.log(canvas.offsetTop);
+const ctx = canvas.getContext("2d");
+let coord = { x: 0, y: 0 };
+
+var resize=()=>{
+  ctx.canvas.width = canvas.width;
+  ctx.canvas.height = canvas.height;
+}
+var reposition=(event)=>{
+  coord.x = event.offsetX;
+  coord.y = event.offsetY; 
+}
+var start=(event)=>{
+  document.addEventListener("mousemove", draw);
+  reposition(event);
+  console.log(event);
+}
+var stop=()=>{
+  document.removeEventListener("mousemove", draw);
+}
+var draw=(event)=>{
+  ctx.beginPath();
+  ctx.lineWidth = 4;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "black";
+  ctx.moveTo(coord.x, coord.y);
+  reposition(event);
+  ctx.lineTo(coord.x, coord.y);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousedown", start);
+canvas.addEventListener("mouseup", stop);
+window.addEventListener("resize", resize);
+
+resize();
+*/
+
+
+const canvas = document.getElementsByClassName('signature-pad')[0];
+const ctx = canvas.getContext('2d');
+	
+function resize(){
+  ctx.canvas.width = canvas.width;
+  ctx.canvas.height = canvas.height;
+}
+	
+let coord = {x:0 , y:0};
+let paint = false;
+	
+function getPosition(event){
+  coord.x = event.offsetX;
+  coord.y = event.offsetY;
+}
+
+function startPainting(event){
+  paint = true;
+  getPosition(event);
+  console.log(event);
+}
+
+function stopPainting(){
+  paint = false;
+}
+	
+function sketch(event){
+  if (!paint) return;
+  ctx.beginPath();
+  ctx.lineWidth = 4;
+  ctx.lineCap = 'round';
+  ctx.strokeStyle = 'black';
+  ctx.moveTo(coord.x, coord.y);
+  getPosition(event);
+  ctx.lineTo(coord.x , coord.y);
+  ctx.stroke();
+}
+
+resize(); // Resizes the canvas once the window loads
+document.addEventListener('mousedown', startPainting);
+document.addEventListener('mouseup', stopPainting);
+document.addEventListener('mousemove', sketch);
+window.addEventListener('resize', resize);
