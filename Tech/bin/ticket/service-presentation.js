b8ca60846f4524c77a21e0aa84484144f7a962ab
cvogel3434 <cvogel.a.m@gmail.com>
2022-12-13 00:50:00 -0600

    init presenestation structure

--- Tech/bin/ticket/service-presentation.js
+++ Tech/bin/ticket/service-presentation.js
@@ -1,40 +1,100 @@
+class ServicePresentation{
+  constructor(){
 
-/*
-const canvas = document.getElementsByClassName('signature-pad')[0];
-console.log(canvas.offsetTop);
-const ctx = canvas.getContext("2d");
-let coord = { x: 0, y: 0 };
+  }
 
-var resize=()=>{
-  ctx.canvas.width = canvas.width;
-  ctx.canvas.height = canvas.height;
-}
-var reposition=(event)=>{
-  coord.x = event.offsetX;
-  coord.y = event.offsetY;
-}
-var start=(event)=>{
-  document.addEventListener("mousemove", draw);
-  reposition(event);
-  console.log(event);
-}
-var stop=()=>{
-  document.removeEventListener("mousemove", draw);
-}
-var draw=(event)=>{
-  ctx.beginPath();
-  ctx.lineWidth = 4;
-  ctx.lineCap = "round";
-  ctx.strokeStyle = "black";
-  ctx.moveTo(coord.x, coord.y);
-  reposition(event);
-  ctx.lineTo(coord.x, coord.y);
-  ctx.stroke();
-}
+  dom = {
+    cont: 'wo-presentation-cont',
+    head: 'wo-presentaiton-header',
+    button:{
+      open:'button-open-presentation'
+    },
+    contract: cntrctform, //from vg-membership.js
+    memlevel:'wo-present-membership',
+    systems: 'wo-present-systems',
+    system: {
+      cont: 'wo-present-system',
+      id: 'wo-present-system-id',
+      repairs: 'wo-present-system-repairs',
+      specials:{
+        diagnostic:'wo-present-repair-diagnostic'
+      },
+      repair: {
+        unapproved:'wo-present-repair-unapproved',
+        cont: 'wo-present-repair',
+        num: 'present-repair-num',
+        desc: 'present-repair-desc',
+        invst: 'present-repair-price',
+        savings: 'present-repair-savings',
+        appr: 'present-repair-appr'
+      }
+    },
+    invest: {
+      savings: 'wo-present-savings-today',
+      regprice: 'wo-present-regprice-today',
+      memprice: 'wo-present-memprice-today',
+      conmonth: 'wo-present-contract-monthly'
+    }
+  }
 
-canvas.addEventListener("mousedown", start);
-canvas.addEventListener("mouseup", stop);
-window.addEventListener("resize", resize);
+  contents=``
 
-resize();
-*/
+  SETpresent = (wodata) => {
+    console.log(wodata);
+    if (wodata.sitems != null) {
+      let slist = document.getElementById(prsdom.systems);
+
+      let rprice=0; //temp for reg book price
+      let mprice=0; //temp for mem book price
+      slist.innerHTML = '';
+
+      for (let x = 0; x < wodata.sitems.length; x++) {
+        let s = slist.appendChild(document.createElement('div'));
+        s.classList.add(prsdom.system.cont);
+        s.appendChild(document.createElement('div')).innerText = this.wo.systems[x].id;
+        let rlist = s.appendChild(document.createElement('div'));
+        rlist.classList.add(prsdom.system.repairs);
+
+        for (let y = 0; y < wodata.repairs[x].length; y++) {
+          rprice = 0;
+          mprice = 0;
+          //tell what column repcost goes to
+
+          //find the memebership cost (if any)
+
+          //fill the difference
+          let r = rlist.appendChild(document.createElement('div'));
+
+          r.classList.add(prsdom.system.repair.cont);
+          r.appendChild(document.createElement('div')).innerText = wodata.repairs[x][y].desc;
+
+          rprice = this.GETbookprice(wodata.repairs[x][y].task,this.wo.reg);
+          r.appendChild(document.createElement('div')).innerText =  rprice;
+          this.wo.build.regprice += (wodata.repairs[x][y].appr ? rprice : 0);
+
+          if(wodata.repairs[x][y].task=='DIAG'){ //special case for diagnostic fee
+            if(this.wo.hascntrct){
+              mprice = this.GETbookprice(wodata.repairs[x][y].task,this.wo.cntrct);
+            }else{mprice = this.GETbookprice(wodata.repairs[x][y].task,this.wo.reg);}
+          }else{mprice = this.GETbookprice(wodata.repairs[x][y].task,this.wo.cntrct);}
+
+          r.appendChild(document.createElement('div')).innerText = mprice;
+          this.wo.build.memprice += (wodata.repairs[x][y].appr ? mprice : 0);
+          r.appendChild(document.createElement('div')).innerText = rprice - mprice;
+          this.wo.build.savings += (wodata.repairs[x][y].appr ? rprice - mprice :0);
+
+          if(!wodata.repairs[x][y].appr){
+            r.classList.add(prsdom.system.repair.unapproved);
+          }
+          r.appendChild(document.createElement('div')).innerText = wodata.repairs[x][y].appr ? 'YES':'NO';
+        }
+      }
+      document.getElementById(prsdom.memlevel).innerText = this.rewardform.GETmemhead(document.getElementById(prsdom.contract.form.name).value) || this.wo.cntrct;
+      document.getElementById(prsdom.invest.regprice).innerText = this.wo.build.regprice;
+      document.getElementById(prsdom.invest.memprice).innerText = this.wo.build.memprice;
+      document.getElementById(prsdom.invest.savings).innerText = this.wo.build.savings;
+      document.getElementById(prsdom.invest.conmonth).innerText = this.rewardform.GETformprice();
+    }
+  }
+
+}
