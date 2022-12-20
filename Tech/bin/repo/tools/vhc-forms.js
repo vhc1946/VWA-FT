12e6b7e619da97ee199a20bc1bc710bb45d58e04
cvogel3434 <cvogel.a.m@gmail.com>
2022-12-20 00:30:12 -0600

    modified the way for looks for tagName

--- Tech/bin/repo/tools/vhc-forms.js
+++ Tech/bin/repo/tools/vhc-forms.js
@@ -9,26 +9,22 @@ class VHCform{
   set form(input={}){
     for(let i in this.inputs){
       try{
-
-      console.log(i, this.inputs[i].value)
         switch(this.inputs[i].tagName){
           case 'INPUT':{this.inputs[i].value=input[i]?input[i]:'';break;}
           case 'TEXTAREA':{this.inputs[i].value=input[i]?input[i]:'';break;}
           default:{this.inputs[i].innerText = input[i]?input[i]:'';}
         }
-      }catch{console.log(this.inputs[i]);console.log(`${i} is not setup in the form`)}
+      }catch{console.log(`${i} is not setup in the form`)}
     }
   }
   get form(){
-    console.log('here')
     let fi ={}
     for(let i in this.inputs){
       try{
         switch(this.inputs[i].tagName){
           case 'INPUT':{fi[i]=this.inputs[i].value;break;}
           case 'TEXTAREA':{fi[i]=this.inputs[i].value;break;}
-          case 'DIV':{fi[i]=this.inputs[i].innerText;break;}
-          default:{console.log(`${i} failed to get from form`);}
+          default:{fi[i]=this.inputs[i].innerText;break;}
         }
       }catch{console.log(`${i} failed to get from form`);}
     }
